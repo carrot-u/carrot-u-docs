686b381b605f8519c0788ec847d807e32c776e6c
Howard Fong <howard.fong@instacart.com>
2020-01-21 09:03:26 -0800

    adding login

--- 2019-session/lectures/rails/instacartlite/config/routes.rb
+++ 2019-session/lectures/rails/instacartlite/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
--- 2019-session/lectures/rails/instacartlite/db/schema.rb
+++ 2019-session/lectures/rails/instacartlite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_062014) do
+ActiveRecord::Schema.define(version: 2020_01_21_155247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_062014) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
