96033629f0c25e768db2bc543a4ce8d7eef8fd46
Viktor Evdokimov <viktor.evdokimov@instacart.com>
2020-02-04 01:35:35 -0800

    improve rspec setup

--- 2019-session/lectures/rails/instacartlite/spec/controllers/welcome_controller_spec.rb
+++ 2019-session/lectures/rails/instacartlite/spec/controllers/welcome_controller_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-	render_views
+  render_views
 
-	describe "#index" do
+  describe '#index' do
+    subject { get :index }
 
-		it "works" do
-			get :index
-			expect(response.body).to include "Groceries" #("Groceries delivered in as lite-le as 1 hour")
+    it 'is expected to render text "Groceries delivered"' do
+      subject
+      expect(response.body).to include('Groceries delivered in as lite-le as 1 hour')
+    end
 
-			# have_content("")
-		end
-	end
-
+    it { is_expected.to have_http_status :ok }
+    it { is_expected.to render_template :index }
+  end
 end
